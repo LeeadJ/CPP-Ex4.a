b4cd1ea8b19838e1402ccf54fe3dedde81191ff0
LeeadJ <leead123@gmail.com>
2022-04-27 23:52:42 +0300

    added block

--- Ambassador.cpp
+++ Ambassador.cpp
@@ -1,14 +1,19 @@
 #include "Ambassador.hpp"
 
 namespace coup{
-    //Constructor
+    //Constructor:
     Ambassador::Ambassador(Game game, std::string name) : Player(game, name){
         this->_myRole="Ambassador";
     }
     
 
-    //functions
+    //Functions:
+    //Transfers a coin from ont player to another.
     void Ambassador::transfer(Player& p1, Player& p2) const{
+    
+    }
+    //Blocks the Captain from stealing 2 coins.
+    void Ambassador::block(Player& p){
 
     }
 }
--- Ambassador.hpp
+++ Ambassador.hpp
@@ -5,11 +5,15 @@ namespace coup{
     class Ambassador : public Player
     {
         public:
-        //Constructor
+        //Constructor:
         Ambassador(Game game, std::string name);
 
-        //functions
+        //Functions:
+        //Transfers a coin from ont player to another.
         void transfer(Player& p1, Player& p2) const;
+        //Blocks the Captain from stealing 2 coins.
+        void block(Player& p);
+        
 
     };
 }
