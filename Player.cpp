ba9c1952c5ff82abd7107f3c9958abe236cdc3eb
LeeadJ <leead123@gmail.com>
2022-05-03 17:19:46 +0300

    updated Player

--- Player.cpp
+++ Player.cpp
@@ -1,42 +1,53 @@
 #include "Player.hpp"
 
 namespace coup{
+    //Setter:
+    void Player::setPlayer(const Game& game, const std::string& name){
+        this->setName(name);
+        this->setGame(game);
+        this->setCoins(0);
+        this->setRole("Default Player");
+    }
     //Construcor
-        Player::Player(const Game& game, const std::string& name)
-        {
-            this->_myName=name;
-            this->_myCoins=0;
-            this->_myGame = game;
-            this->_myGame.players().push_back(name);
-        }
+    Player::Player(){
+        this->setName("Default Name");
+        this->setCoins(0);
+        this->setRole("Default Player");
+    }
+    Player::Player(const Game& game, const std::string& name){
+        this->setPlayer(game, name);
+        this->_myGame.players().push_back(name);
+    }
+    Player::Player(const Player& other){
+        this->setPlayer(other.getGame(), other.getName());
+        this->setCoins(other.getCoins());
+        this->setRole(other.getRole());
+    }
 
-        
-        //functions:
-        //Increases the players coins by 1:
-        void Player::income()
-        {
-            this->_myCoins++;
-        }
-        
-        //Increases the players coins by 2:
-        void Player::foreign_aid()
-        {
-            this->_myCoins+=2;
-        }
+    
+    //Functions:
+    //Increases the players coins by 1:
+    void Player::income(){
+        this->setCoins(this->getCoins()+1);
+    }
+    
+    //Increases the players coins by 2:
+    void Player::foreign_aid(){
+        this->setCoins(this->getCoins()+2);
+    }
 
-        //Eliminate a Player from his Role and remove him from the Game:
-        void Player::coup(Player& p)
-        {
-            std::cout<< "Player::coup()" << std::endl;
-        }
+    //Eliminate a Player from his Role and remove him from the Game:
+    void Player::coup(Player& p){
+        std::cout<< "Player::coup()" << std::endl;
+    }
 
-        //Returns the Players role:
-        std::string Player::role() const{
-            return this->_myRole;
-        }
+    //Returns the Players role:
+    std::string Player::role() const{
+        return this->getRole();
+    }
 
-        //Returns the Players coin count:
-        int Player::coins() const{
-            return this->_myCoins;
-        }
+    //Returns the Players coin count:
+    int Player::coins() const{
+        return this->getCoins();
+    }
 }
--- Player.hpp
+++ Player.hpp
@@ -5,29 +5,46 @@
 
 namespace coup{
     class Player{
-        public:
+        
         std::string _myName;
         std::string _myRole;
         int _myCoins;
         Game _myGame;
 
-        //Construcor
-        Player(const Game& game, const std::string& name);
-        
-        //functions:
-        //Increases the players coins by 1:
-        void income();
-        
-        //Increases the players coins by 2:
-        void foreign_aid();
+        public:
+            //Getters:
+            std::string getName() const {return this->_myName;}
+            std::string getRole() const {return this->_myRole;}
+            int getCoins() const {return this->_myCoins;}
+            Game getGame() const {return this->_myGame;}
 
-        //Eliminate a Player from his Role and remove him from the Game:
-        static void coup(Player& p);
+            //Setters:
+            void setName(const std::string& n) {this->_myName=n;}
+            void setRole(const std::string& r) {this->_myRole=r;}
+            void setCoins(int num) {this->_myCoins=num;}
+            void setGame(const Game& g) {this->_myGame=g;}
+            void setPlayer(const Game& game, const std::string& name);
 
-        //Returns the Players role:
-        std::string role() const;
 
-        //Returns the Players coin count:
-        int coins() const;
+            //Constructor
+            Player();
+            Player(const Game& game, const std::string& name);
+            Player(const Player& other);
+            
+            //functions:
+            //Increases the players coins by 1:
+            void income();
+            
+            //Increases the players coins by 2:
+            void foreign_aid();
+
+            //Eliminate a Player from his Role and remove him from the Game:
+            static void coup(Player& p);
+
+            //Returns the Players role:
+            std::string role() const;
+
+            //Returns the Players coin count:
+            int coins() const;
     };
 }
