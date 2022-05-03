c69552a45df6a5dc59876804f1606a6231ba058b
LeeadJ <leead123@gmail.com>
2022-05-03 21:18:51 +0300

    Player update

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.hpp"
 
+
 namespace coup{
     //Setter:
     void Player::setPlayer(const Game& game, const std::string& name){
@@ -7,6 +8,7 @@ namespace coup{
         this->setGame(game);
         this->setCoins(0);
         this->setRole("Default Player");
+        this->setPreviousTurn("Null");
     }
     //Construcor
     Player::Player(){
@@ -20,25 +22,49 @@ namespace coup{
     }
     Player::Player(const Player& other){
         this->setPlayer(other.getGame(), other.getName());
-        this->setCoins(other.getCoins());
-        this->setRole(other.getRole());
+        this->setCoins(other.coins());
+        this->setRole(other.role());
     }
 
     
     //Functions:
     //Increases the players coins by 1:
     void Player::income(){
-        this->setCoins(this->getCoins()+1);
+        if(this->getGame().turn().compare(this->getName()) != 0){
+            throw std::runtime_error("Player income() Error: Not Players turn.");
+        }
+        this->setCoins(this->coins()+1);
+        this->setPreviousTurn("income");
     }
     
     //Increases the players coins by 2:
     void Player::foreign_aid(){
-        this->setCoins(this->getCoins()+2);
+        if(this->getGame().turn().compare(this->getName()) != 0){
+            throw std::runtime_error("Player foreign_aid() Error: Not Players turn.");
+        }
+        this->setCoins(this->coins()+2);
+        this->setPreviousTurn("foreign_aid");
     }
 
-    //Eliminate a Player from his Role and remove him from the Game:
+    //Eliminates a Player from the Game:
     void Player::coup(Player& p){
-        std::cout<< "Player::coup()" << std::endl;
+        const std::vector<Player>& v = this->getGame().getPlayersVec(); 
+        if(std::find(v.begin(), v.end(), p) == v.end()){
+            throw std::runtime_error("Player coup() Error: Player to coup not in the game.");
+        }
+        if(this->coins() < 7){
+            throw std::runtime_error("Player coup() Error: Not enough coins to use coup().");
+        }
+        for(int i=0; i<this->getGame().getGameSize(); i++){
+            if(&(this->getGame().getPlayersVec()[i]) == &p){
+                this->getGame().getPlayerNameVec().erase(this->getGame().getPlayerNameVec().begin()+i); //Erasing the name from the nameVec
+                this->getGame().getPlayersVec().erase(this->getGame().getPlayersVec().begin()+i); //Erasing from playersVec
+                this->getGame().addPlayer_coupMap(p);
+                break;
+            }
+        } 
+        this->setCoins(this->coins()-7);
+        this->setPreviousTurn("coup");
     }
 
     //Returns the Players role:
--- Player.hpp
+++ Player.hpp
@@ -10,6 +10,8 @@ namespace coup{
         std::string _myRole;
         int _myCoins;
         Game _myGame;
+        std::string _previousTurn;
+        
 
         public:
             //Getters:
@@ -17,13 +19,16 @@ namespace coup{
             std::string getRole() const {return this->_myRole;}
             int getCoins() const {return this->_myCoins;}
             Game getGame() const {return this->_myGame;}
+            std::string getPreviousTurn() const {return this->_previousTurn;}
 
             //Setters:
             void setName(const std::string& n) {this->_myName=n;}
             void setRole(const std::string& r) {this->_myRole=r;}
             void setCoins(int num) {this->_myCoins=num;}
             void setGame(const Game& g) {this->_myGame=g;}
+            void setPreviousTurn(const std::string turn) {this->_previousTurn=turn;}
             void setPlayer(const Game& game, const std::string& name);
+            
 
 
             //Constructor
@@ -38,13 +43,13 @@ namespace coup{
             //Increases the players coins by 2:
             void foreign_aid();
 
-            //Eliminate a Player from his Role and remove him from the Game:
-            static void coup(Player& p);
+            //Eliminates a Player from the Game:
+            void coup(Player& p);
 
             //Returns the Players role:
             std::string role() const;
 
-            //Returns the Players coin count:
+            //Returns the amount of coins the Player has:
             int coins() const;
     };
 }
